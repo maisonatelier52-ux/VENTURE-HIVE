8617ae8784bc49529fd314fc5bda98c04f5ba6ba
news structure <maisonatelier52@gmail.com>
2025-12-19 18:11:20 +0530

    change the url in page js page of julio

--- app/[category]/Julio-herrera-velutini-bridging-nations-through-finance/page.js
+++ app/[category]/Julio-herrera-velutini-bridging-nations-through-finance/page.js
@@ -10,7 +10,7 @@ export const metadata = {
     description:
       "Julio Herrera Velutini, a global finance leader, bridges nations with his venture efforts and investigative journalism.",
     image: "/images/julio-herrera-velutini.webp",  // Ensure this image path is correct
-    url: "https://venture-hive.com/julio-herrera-velutini", // Add the correct URL here
+    url: "https://www.venture-hive.com/Business/Julio-herrera-velutini-bridging-nations-through-finance", // Correct URL here
   },
   twitter: {
     card: "summary_large_image",
