800c6fc181f96d141de0e0520a49d539e4681cf1
news structure <maisonatelier52@gmail.com>
2025-12-20 15:40:04 +0530

    chnage the meta detail of pillar 2 content

--- app/julio-herrera-velutini/[slug]/page.jsx
+++ app/julio-herrera-velutini/[slug]/page.jsx
@@ -8,16 +8,19 @@ export async function generateMetadata({ params }) {
   const article = pillarContents?.find((item) => item.slug === slug);
 
   if (!article) return {};
+
+   const title = article.metaTitle || 'Default Title'; // Fallback if no title exists
+  const description = article.metaDescription || 'Default Description'; // Fallback if no description exists
  
   return {
-    title: article.metaTitle,
-    description: article.metaDescription,
+    title:title,
+    description: description,
     alternates: {
       canonical: `https://venture-hive.com/julio-herrera-velutini/${slug}`,
     },
     openGraph: {
-      title: article.metaTitle,
-      description: article.metaDescription,
+      title: title,
+      description: description,
       url: `https://venture-hive.com/julio-herrera-velutini/${slug}`,
       siteName: "Venture Hive",
       images: [
@@ -32,8 +35,8 @@ export async function generateMetadata({ params }) {
     },
     twitter: {
       card: "summary_large_image",
-      title: article.metaTitle,
-      description: article.metaDescription,
+      title: title,
+      description: description,
       images: [`https://venture-hive.com${article.hero.imageSrc}`],
     },
   };
