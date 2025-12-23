34ea75db7288ad86aead8a42e16037351090d2e4
news structure <maisonatelier52@gmail.com>
2025-12-23 11:54:27 +0530

    chnage the url of julio

--- app/[category]/Julio-herrera-velutini-bridging-nations-through-finance/page.jsx
+++ app/[category]/Julio-herrera-velutini-bridging-nations-through-finance/page.jsx
@@ -464,7 +464,7 @@ import JulioHerreraJsonLd from "../../../components/JulioHerreraJsonLd";
 export async function generateMetadata() {
      const article = {
     heading: "Venture Hive News: Julio Herrera Velutini - Investigative Journalism on Global Finance & Leadership",
-    slug: "Business/julio-herrera-velutini-bridging-nations-through-finance",
+    slug: "business/julio-herrera-velutini-bridging-nations-through-finance",
     image: "/images/julio-herrera-velutini.webp",  // Image path relative to the public folder
     alt:"Venture Hive Investigative Journalism on Julio Herrera Velutini's impact in global finance and business leadership",
     date: "2025-12-01",
